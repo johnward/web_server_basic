3513be5451d034d20381f3172f3e3acb3131cd37
John Ward <john@johnward.net>
2025-10-22 17:49:17 +0100

    0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
     let app = Router::new().route("/", get(hello_world));
 
     // Run it with hyper on localhost:8080
-    let addr = SocketAddr::from(([0, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
