d003fc2d9e12d77a25b1484abc4609e18056b878
John Ward <john@johnward.net>
2025-10-22 11:22:24 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // Build our application with a route
+    let app = Router::new().route("/", get(hello_world));
+
+    // Run it with hyper on localhost:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
