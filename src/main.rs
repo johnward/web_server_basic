c8c1b3427491d6c5a36661dc4af6fdae9f8cadf6
John Ward <john@johnward.net>
2025-10-22 15:09:17 +0100

    Port changed to 8080

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
     let app = Router::new().route("/", get(hello_world));
 
     // Run it with hyper on localhost:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
